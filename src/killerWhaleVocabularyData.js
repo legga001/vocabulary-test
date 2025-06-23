b25634b355c9634e7a09c3e296ff2b3cba3ee813
legga001 <legga001@gmail.com>
2025-06-23 18:55:16 +0100

    Create killerWhaleVocabularyData.js

---
+++ src/killerWhaleVocabularyData.js
@@ -0,0 +1,92 @@
+// src/killerWhaleVocabularyData.js
+// Article: "Killer whales 'massage' each other using kelp"
+// Date: 23 June 2025
+// Source: BBC News
+
+export const getKillerWhaleVocabularyQuestions = () => {
+  return [
+    {
+      id: 1,
+      sentence: "Orcas in the North Pacific have been seen massaging each other - rubbing pieces of kelp between their _______.",
+      answer: "bodies",
+      hint: "The main part of an animal or person",
+      level: "A2"
+    },
+    {
+      id: 2,
+      sentence: "Using drones, researchers filmed the animals selecting and biting off the kelp, then placing the _______ piece of seaweed onto the back of another whale.",
+      answer: "tube-shaped",
+      hint: "Having the form of a hollow cylinder",
+      level: "B2"
+    },
+    {
+      id: 3,
+      sentence: "The scientists think the massages might have a health or _______ function.",
+      answer: "hygiene",
+      hint: "Practices that keep things clean and healthy",
+      level: "B1"
+    },
+    {
+      id: 4,
+      sentence: "The findings appear to be an example of tool use in killer whales - with the animals _______ a tubular piece of seaweed.",
+      answer: "fashioning",
+      hint: "Making or shaping something with skill",
+      level: "C1"
+    },
+    {
+      id: 5,
+      sentence: "The Center for Whale Research team have special _______ to study the whales up close.",
+      answer: "permits",
+      hint: "Official documents giving permission to do something",
+      level: "B1"
+    },
+    {
+      id: 6,
+      sentence: "This could be a marine version of what is known as _______ in primates.",
+      answer: "allogrooming",
+      hint: "When animals groom other members of their group",
+      level: "C1"
+    },
+    {
+      id: 7,
+      sentence: "Many monkeys and apes groom other members of their groups - spending hours picking through fur, cleaning and removing _______.",
+      answer: "parasites",
+      hint: "Small organisms that live on or in other animals",
+      level: "B2"
+    },
+    {
+      id: 8,
+      sentence: "The researchers have _______ this behaviour as 'allokelping'.",
+      answer: "dubbed",
+      hint: "Given a name or title to something",
+      level: "B2"
+    },
+    {
+      id: 9,
+      sentence: "There was some indication that whales with 'more peely skin' were more likely to _______ in the massages.",
+      answer: "engage",
+      hint: "To take part in or become involved with",
+      level: "B1"
+    },
+    {
+      id: 10,
+      sentence: "The study of this _______ killer whale population was started by Dr Ken Balcomb more than 50 years ago.",
+      answer: "threatened",
+      hint: "In danger of becoming extinct or harmed",
+      level: "B2"
+    }
+  ];
+};
+
+export const getKillerWhaleArticleInfo = () => {
+  return {
+    title: "Killer whales 'massage' each other using kelp",
+    url: "https://www.bbc.co.uk/news/articles/cwyqll5n2qro",
+    source: "BBC News",
+    date: "23 June 2025",
+    author: "Victoria Gill",
+    summary: "Researchers have discovered that orcas in the North Pacific use kelp to 'massage' each other, fashioning pieces of seaweed into tools for what appears to be both social bonding and hygiene purposes.",
+    image: "Aerial view of killer whales using kelp",
+    readingTime: "3-4 minutes"
+  };
+};
