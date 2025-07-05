178a12daf78abe32c968d5bf6b37925507b8e7bb
legga001 <legga001@gmail.com>
2025-07-05 18:06:43 +0100

    Update pronunciationData.js

--- src/pronunciationData.js
+++ src/pronunciationData.js
@@ -1,65 +1,583 @@
-// Additional pronunciation entries for src/pronunciationData.js - Zooplankton vocabulary
+// src/pronunciationData.js - COMPLETE FIXED VERSION
+// Comprehensive pronunciation data for all vocabulary words
 
-// Add these entries to the existing pronunciationData object:
+export const pronunciationData = {
+  // ==============================================
+  // A2 LEVEL WORDS (25 words)
+  // ==============================================
+  "buy": {
+    ipa: "/baɪ/",
+    phonetic: "BUY"
+  },
+  "hot": {
+    ipa: "/hɒt/",
+    phonetic: "HOT"
+  },
+  "happy": {
+    ipa: "/ˈhæpi/",
+    phonetic: "HAP-ee"
+  },
+  "works": {
+    ipa: "/wɜːks/",
+    phonetic: "WURKS"
+  },
+  "clean": {
+    ipa: "/kliːn/",
+    phonetic: "KLEEN"
+  },
+  "garden": {
+    ipa: "/ˈɡɑːdn/",
+    phonetic: "GAR-den"
+  },
+  "wake": {
+    ipa: "/weɪk/",
+    phonetic: "WAYK"
+  },
+  "read": {
+    ipa: "/riːd/",
+    phonetic: "REED"
+  },
+  "car": {
+    ipa: "/kɑː/",
+    phonetic: "KAR"
+  },
+  "dinner": {
+    ipa: "/ˈdɪnə/",
+    phonetic: "DIN-er"
+  },
+  "closed": {
+    ipa: "/kləʊzd/",
+    phonetic: "KLOHZD"
+  },
+  "wore": {
+    ipa: "/wɔː/",
+    phonetic: "WAWR"
+  },
+  "sleeping": {
+    ipa: "/ˈsliːpɪŋ/",
+    phonetic: "SLEE-ping"
+  },
+  "take": {
+    ipa: "/teɪk/",
+    phonetic: "TAYK"
+  },
+  "food": {
+    ipa: "/fuːd/",
+    phonetic: "FOOD"
+  },
+  "favourite": {
+    ipa: "/ˈfeɪvərɪt/",
+    phonetic: "FAY-ver-it"
+  },
+  "water": {
+    ipa: "/ˈwɔːtə/",
+    phonetic: "WAW-ter"
+  },
+  "hair": {
+    ipa: "/heə/",
+    phonetic: "HAIR"
+  },
+  "meal": {
+    ipa: "/miːl/",
+    phonetic: "MEEL"
+  },
+  "new": {
+    ipa: "/njuː/",
+    phonetic: "NYOO"
+  },
+  "door": {
+    ipa: "/dɔː/",
+    phonetic: "DAWR"
+  },
+  "live": {
+    ipa: "/lɪv/",
+    phonetic: "LIV"
+  },
+  "train": {
+    ipa: "/treɪn/",
+    phonetic: "TRAYN"
+  },
+  "present": {
+    ipa: "/ˈpreznt/",
+    phonetic: "PREZ-ent"
+  },
+  "weather": {
+    ipa: "/ˈweðə/",
+    phonetic: "WEH-ther"
+  },
 
-// Zooplankton article vocabulary
-"gorge": {
-  ipa: "/ɡɔːdʒ/",
-  phonetic: "GAWRJ"
-},
-"unsung": {
-  ipa: "/ʌnˈsʌŋ/",
-  phonetic: "uhn-SUNG"
-},
-"epic": {
-  ipa: "/ˈepɪk/",
-  phonetic: "EP-ik"
-},
-"equivalent": {
-  ipa: "/ɪˈkwɪvələnt/",
-  phonetic: "i-KWIV-uh-luhnt"
-},
-"transports": {
-  ipa: "/ˈtrænspɔːts/",
-  phonetic: "TRANS-pawrts"
-},
-"sausages": {
-  ipa: "/ˈsɒsɪdʒɪz/",
-  phonetic: "SOS-i-jiz"
-},
-"astounded": {
-  ipa: "/əˈstaʊndɪd/",
-  phonetic: "uh-STOWND-id"
-},
-"re-think": {
-  ipa: "/riːˈθɪŋk/",
-  phonetic: "ree-THINK"
-},
-"harvesting": {
-  ipa: "/ˈhɑːvɪstɪŋ/",
-  phonetic: "HAHR-vis-ting"
-},
-"remarkable": {
-  ipa: "/rɪˈmɑːkəbl/",
-  phonetic: "ri-MAHR-kuh-buhl"
-},
-"zooplankton": {
-  ipa: "/ˈzuːplæŋktən/",
-  phonetic: "ZOO-plank-tuhn"
-},
-"copepods": {
-  ipa: "/ˈkəʊpɪpɒdz/",
-  phonetic: "KOH-pi-podz"
-},
-"migration": {
-  ipa: "/maɪˈɡreɪʃn/",
-  phonetic: "my-GRAY-shuhn"
-},
-"carbon": {
-  ipa: "/ˈkɑːbən/",
-  phonetic: "KAHR-buhn"
-},
-"sequestration": {
-  ipa: "/ˌsiːkwɪˈstreɪʃn/",
-  phonetic: "see-kwi-STRAY-shuhn"
-}
+  // ==============================================
+  // B1 LEVEL WORDS (35 words)
+  // ==============================================
+  "sad": {
+    ipa: "/sæd/",
+    phonetic: "SAD"
+  },
+  "hire": {
+    ipa: "/ˈhaɪə/",
+    phonetic: "HY-er"
+  },
+  "finish": {
+    ipa: "/ˈfɪnɪʃ/",
+    phonetic: "FIN-ish"
+  },
+  "cancelled": {
+    ipa: "/ˈkænsəld/",
+    phonetic: "KAN-seld"
+  },
+  "avoid": {
+    ipa: "/əˈvɔɪd/",
+    phonetic: "uh-VOYD"
+  },
+  "study": {
+    ipa: "/ˈstʌdi/",
+    phonetic: "STUD-ee"
+  },
+  "decided": {
+    ipa: "/dɪˈsaɪdɪd/",
+    phonetic: "di-SY-ded"
+  },
+  "includes": {
+    ipa: "/ɪnˈkluːdz/",
+    phonetic: "in-KLOODZ"
+  },
+  "change": {
+    ipa: "/tʃeɪndʒ/",
+    phonetic: "CHAYNJ"
+  },
+  "advised": {
+    ipa: "/ədˈvaɪzd/",
+    phonetic: "ad-VYZD"
+  },
+  "bring": {
+    ipa: "/brɪŋ/",
+    phonetic: "BRING"
+  },
+  "delayed": {
+    ipa: "/dɪˈleɪd/",
+    phonetic: "di-LAYD"
+  },
+  "enjoys": {
+    ipa: "/ɪnˈdʒɔɪz/",
+    phonetic: "in-JOYZ"
+  },
+  "return": {
+    ipa: "/rɪˈtɜːn/",
+    phonetic: "ri-TURN"
+  },
+  "excited": {
+    ipa: "/ɪkˈsaɪtɪd/",
+    phonetic: "ik-SY-ted"
+  },
+  "improve": {
+    ipa: "/ɪmˈpruːv/",
+    phonetic: "im-PROOV"
+  },
+  "serves": {
+    ipa: "/sɜːvz/",
+    phonetic: "SURVZ"
+  },
+  "consider": {
+    ipa: "/kənˈsɪdə/",
+    phonetic: "kuhn-SID-er"
+  },
+  "requires": {
+    ipa: "/rɪˈkwaɪəz/",
+    phonetic: "ri-KWY-erz"
+  },
+  "misses": {
+    ipa: "/ˈmɪsɪz/",
+    phonetic: "MIS-ez"
+  },
+  "covers": {
+    ipa: "/ˈkʌvəz/",
+    phonetic: "KUV-erz"
+  },
+  "organise": {
+    ipa: "/ˈɔːɡənaɪz/",
+    phonetic: "AWR-guh-nyz"
+  },
+  "forecast": {
+    ipa: "/ˈfɔːkɑːst/",
+    phonetic: "FAWR-kast"
+  },
+  "attend": {
+    ipa: "/əˈtend/",
+    phonetic: "uh-TEND"
+  },
+  "provides": {
+    ipa: "/prəˈvaɪdz/",
+    phonetic: "pruh-VYDZ"
+  },
+  "refused": {
+    ipa: "/rɪˈfjuːzd/",
+    phonetic: "ri-FYOOZD"
+  },
+  "affect": {
+    ipa: "/əˈfekt/",
+    phonetic: "uh-FEKT"
+  },
+  "discuss": {
+    ipa: "/dɪˈskʌs/",
+    phonetic: "dis-KUSS"
+  },
+  "check": {
+    ipa: "/tʃek/",
+    phonetic: "CHEK"
+  },
+  "offers": {
+    ipa: "/ˈɒfəz/",
+    phonetic: "OF-erz"
+  },
+  "succeeds": {
+    ipa: "/səkˈsiːdz/",
+    phonetic: "suhk-SEEDZ"
+  },
+  "replace": {
+    ipa: "/rɪˈpleɪs/",
+    phonetic: "ri-PLAYS"
+  },
+  "goal": {
+    ipa: "/ɡəʊl/",
+    phonetic: "GOHL"
+  },
+  "prepare": {
+    ipa: "/prɪˈpeə/",
+    phonetic: "pri-PAIR"
+  },
+  "remarkable": {
+    ipa: "/rɪˈmɑːkəbl/",
+    phonetic: "ri-MAHR-kuh-buhl"
+  },
+
+  // ==============================================
+  // B2 LEVEL WORDS (30 words)
+  // ==============================================
+  "significant": {
+    ipa: "/sɪɡˈnɪfɪkənt/",
+    phonetic: "sig-NIF-i-kuhnt"
+  },
+  "analyse": {
+    ipa: "/ˈænəlaɪz/",
+    phonetic: "AN-uh-lyz"
+  },
+  "exceptional": {
+    ipa: "/ɪkˈsepʃənl/",
+    phonetic: "ik-SEP-shuhn-uhl"
+  },
+  "demonstrated": {
+    ipa: "/ˈdemənstreɪtɪd/",
+    phonetic: "DEM-uhn-stray-ted"
+  },
+  "recommendations": {
+    ipa: "/ˌrekəmenˈdeɪʃnz/",
+    phonetic: "rek-uh-men-DAY-shuhnz"
+  },
+  "challenges": {
+    ipa: "/ˈtʃælɪndʒɪz/",
+    phonetic: "CHAL-in-jiz"
+  },
+  "commitment": {
+    ipa: "/kəˈmɪtmənt/",
+    phonetic: "kuh-MIT-muhnt"
+  },
+  "scientific": {
+    ipa: "/ˌsaɪənˈtɪfɪk/",
+    phonetic: "sy-uhn-TIF-ik"
+  },
+  "diplomatic": {
+    ipa: "/ˌdɪpləˈmætɪk/",
+    phonetic: "dip-luh-MAT-ik"
+  },
+  "subtle": {
+    ipa: "/ˈsʌtl/",
+    phonetic: "SUT-uhl"
+  },
+
+  // Zooplankton article vocabulary
+  "gorge": {
+    ipa: "/ɡɔːdʒ/",
+    phonetic: "GAWRJ"
+  },
+  "unsung": {
+    ipa: "/ʌnˈsʌŋ/",
+    phonetic: "uhn-SUNG"
+  },
+  "epic": {
+    ipa: "/ˈepɪk/",
+    phonetic: "EP-ik"
+  },
+  "equivalent": {
+    ipa: "/ɪˈkwɪvələnt/",
+    phonetic: "i-KWIV-uh-luhnt"
+  },
+  "transports": {
+    ipa: "/ˈtrænspɔːts/",
+    phonetic: "TRANS-pawrts"
+  },
+  "sausages": {
+    ipa: "/ˈsɒsɪdʒɪz/",
+    phonetic: "SOS-i-jiz"
+  },
+  "astounded": {
+    ipa: "/əˈstaʊndɪd/",
+    phonetic: "uh-STOWND-id"
+  },
+  "re-think": {
+    ipa: "/riːˈθɪŋk/",
+    phonetic: "ree-THINK"
+  },
+  "harvesting": {
+    ipa: "/ˈhɑːvɪstɪŋ/",
+    phonetic: "HAHR-vis-ting"
+  },
+  "zooplankton": {
+    ipa: "/ˈzuːplæŋktən/",
+    phonetic: "ZOO-plank-tuhn"
+  },
+  "copepods": {
+    ipa: "/ˈkəʊpɪpɒdz/",
+    phonetic: "KOH-pi-podz"
+  },
+  "migration": {
+    ipa: "/maɪˈɡreɪʃn/",
+    phonetic: "my-GRAY-shuhn"
+  },
+  "carbon": {
+    ipa: "/ˈkɑːbən/",
+    phonetic: "KAHR-buhn"
+  },
+  "sequestration": {
+    ipa: "/ˌsiːkwɪˈstreɪʃn/",
+    phonetic: "see-kwi-STRAY-shuhn"
+  },
+
+  // Article-based vocabulary (other articles)
+  "sail": {
+    ipa: "/seɪl/",
+    phonetic: "SAYL"
+  },
+  "hide": {
+    ipa: "/haɪd/",
+    phonetic: "HYD"
+  },
+  "caught": {
+    ipa: "/kɔːt/",
+    phonetic: "KAWT"
+  },
+  "security": {
+    ipa: "/sɪˈkjʊərɪti/",
+    phonetic: "si-KYOOR-i-tee"
+  },
+  "concerning": {
+    ipa: "/kənˈsɜːnɪŋ/",
+    phonetic: "kuhn-SUR-ning"
+  },
+  "recruited": {
+    ipa: "/rɪˈkruːtɪd/",
+    phonetic: "ri-KROO-tid"
+  },
+  "tides": {
+    ipa: "/taɪdz/",
+    phonetic: "TYDZ"
+  },
+  "disappear": {
+    ipa: "/ˌdɪsəˈpɪə/",
+    phonetic: "dis-uh-PEER"
+  },
+  "vulnerabilities": {
+    ipa: "/ˌvʌlnərəˈbɪlɪtiz/",
+    phonetic: "vul-nuh-ruh-BIL-i-teez"
+  },
+  "complexion": {
+    ipa: "/kəmˈplekʃən/",
+    phonetic: "kuhm-PLEK-shuhn"
+  },
+
+  // Reading exercise vocabulary (octopus article)
+  "close": {
+    ipa: "/kləʊz/",
+    phonetic: "KLOHZ"
+  },
+  "pots": {
+    ipa: "/pɒts/",
+    phonetic: "POTS"
+  },
+  "ruining": {
+    ipa: "/ˈruːɪnɪŋ/",
+    phonetic: "ROO-in-ing"
+  },
+  "catching": {
+    ipa: "/ˈkætʃɪŋ/",
+    phonetic: "KACH-ing"
+  },
+  "invasion": {
+    ipa: "/ɪnˈveɪʒən/",
+    phonetic: "in-VAY-zhuhn"
+  },
+  "mediterranean": {
+    ipa: "/ˌmedɪtəˈreɪniən/",
+    phonetic: "med-i-tuh-RAY-nee-uhn"
+  },
+  "shortage": {
+    ipa: "/ˈʃɔːtɪdʒ/",
+    phonetic: "SHAWR-tij"
+  },
+  "emergency": {
+    ipa: "/ɪˈmɜːdʒənsi/",
+    phonetic: "i-MUR-juhn-see"
+  },
+  "juvenile": {
+    ipa: "/ˈdʒuːvənaɪl/",
+    phonetic: "JOO-vuh-nyl"
+  },
+  "welcoming": {
+    ipa: "/ˈwelkəmɪŋ/",
+    phonetic: "WEL-kuh-ming"
+  },
+
+  // Air India article vocabulary
+  "angry": {
+    ipa: "/ˈæŋɡri/",
+    phonetic: "ANG-gree"
+  },
+  "due": {
+    ipa: "/djuː/",
+    phonetic: "DYOO"
+  },
+  "crashed": {
+    ipa: "/kræʃt/",
+    phonetic: "KRASHT"
+  },
+  "disappointed": {
+    ipa: "/ˌdɪsəˈpɔɪntɪd/",
+    phonetic: "dis-uh-POYN-ted"
+  },
+  "frustrated": {
+    ipa: "/frʌˈstreɪtɪd/",
+    phonetic: "fruhs-TRAY-ted"
+  },
+  "dejected": {
+    ipa: "/dɪˈdʒektɪd/",
+    phonetic: "di-JEK-ted"
+  },
+  "requested": {
+    ipa: "/rɪˈkwestɪd/",
+    phonetic: "ri-KWES-ted"
+  },
+  "struggle": {
+    ipa: "/ˈstrʌɡl/",
+    phonetic: "STRUG-uhl"
+  },
+  "debris": {
+    ipa: "/ˈdebriː/",
+    phonetic: "DEB-ree"
+  },
+
+  // Water treatment article vocabulary
+  "worms": {
+    ipa: "/wɜːmz/",
+    phonetic: "WURMZ"
+  },
+  "impact": {
+    ipa: "/ˈɪmpækt/",
+    phonetic: "IM-pakt"
+  },
+  "released": {
+    ipa: "/rɪˈliːst/",
+    phonetic: "ri-LEEST"
+  },
+  "technique": {
+    ipa: "/tekˈniːk/",
+    phonetic: "tek-NEEK"
+  },
+  "replicates": {
+    ipa: "/ˈreplɪkeɪts/",
+    phonetic: "REP-li-kayts"
+  },
+  "conventional": {
+    ipa: "/kənˈvenʃənl/",
+    phonetic: "kuhn-VEN-shuhn-uhl"
+  },
+  "sanitation": {
+    ipa: "/ˌsænɪˈteɪʃn/",
+    phonetic: "san-i-TAY-shuhn"
+  },
+  "organic": {
+    ipa: "/ɔːˈɡænɪk/",
+    phonetic: "awr-GAN-ik"
+  },
+
+  // Killer whale vocabulary
+  "bodies": {
+    ipa: "/ˈbɒdiz/",
+    phonetic: "BOD-eez"
+  },
+  "tube-shaped": {
+    ipa: "/ˈtjuːb ʃeɪpt/",
+    phonetic: "TOOB-shaypt"
+  },
+  "hygiene": {
+    ipa: "/ˈhaɪdʒiːn/",
+    phonetic: "HY-jeen"
+  },
+  "fashioning": {
+    ipa: "/ˈfæʃənɪŋ/",
+    phonetic: "FASH-uhn-ing"
+  },
+  "permits": {
+    ipa: "/pəˈmɪts/",
+    phonetic: "per-MITS"
+  },
+  "allogrooming": {
+    ipa: "/ˈæləʊɡruːmɪŋ/",
+    phonetic: "AL-oh-groo-ming"
+  },
+  "parasites": {
+    ipa: "/ˈpærəsaɪts/",
+    phonetic: "PAIR-uh-syts"
+  },
+  "dubbed": {
+    ipa: "/dʌbd/",
+    phonetic: "DUHBD"
+  },
+  "engage": {
+    ipa: "/ɪnˈɡeɪdʒ/",
+    phonetic: "in-GAYJ"
+  },
+  "threatened": {
+    ipa: "/ˈθretnd/",
+    phonetic: "THRET-uhnd"
+  }
+};
+
+// Helper function to get pronunciation for a word
+export const getPronunciation = (word) => {
+  const normalizedWord = word.toLowerCase().trim();
+  return pronunciationData[normalizedWord] || null;
+};
+
+// Helper function to check if pronunciation exists
+export const hasPronunciation = (word) => {
+  const normalizedWord = word.toLowerCase().trim();
+  return normalizedWord in pronunciationData;
+};
+
+// Get statistics about pronunciation coverage
+export const getPronunciationStats = () => {
+  const totalWords = Object.keys(pronunciationData).length;
+  const levels = {
+    A2: 0,
+    B1: 0,
+    B2: 0,
+    C1: 0,
+    Articles: 0
+  };
+  
+  // This could be enhanced to categorise words by level
+  // For now, just return total count
+  return {
+    totalWords,
+    coverage: `${totalWords} words with pronunciation data`
+  };
+};
